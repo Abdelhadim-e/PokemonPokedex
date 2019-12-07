d67f1bd538c45d1cbd2d806307184afa3c0449ad
Abdelhadim <e.abdelhadim@gmail.com>
2019-12-07 12:46:59 +0100

    Correct bug, ajout fichier Players et Main puis test affichage new Players

---
+++ src/Players.java
@@ -0,0 +1,21 @@
+
+public class Players {
+	
+	// var name : string
+	private String name;
+	
+	
+	//Constructeur :
+	public Players(String name) {
+		this.name = name;
+	}
+	
+	//Getteur name;
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
--- src/PokeGame.java
+++ src/PokeGame.java
@@ -4,6 +4,12 @@ public class PokeGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("debg");
+		
+		// Test players Poké :
+		Players play1 = new Players("Pierre");
+		System.out.println("Player1 : " + play1);
+		
+		
 
 	}
 
