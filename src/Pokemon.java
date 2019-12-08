3bcd26f68cec8cf509ecbcc0291c7fe455a91210
Abdelhadim <e.abdelhadim@gmail.com>
2019-12-08 13:06:53 +0100

    ajout fichier pokemon extends card

---
+++ src/Pokemon.java
@@ -0,0 +1,15 @@
+
+public class Pokemon extends Card{
+	
+	//var 
+	
+	
+	
+	//Class mère Card
+	public Pokemon(String name, String type) {
+		//Recup valeur du constructeur mère
+		super(name, type);
+	}
+	
+	
+}
