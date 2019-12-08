9253428dec8accbb149ea303b8ed0dc1106ac520
Abdelhadim <e.abdelhadim@gmail.com>
2019-12-08 13:42:42 +0100

    Param Contruct Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -2,14 +2,26 @@
 public class Pokemon extends Card{
 	
 	//var 
-	
+	private int lifePoints;
+	private String firstElement;
+	private String secondElement;
+	private int stage;
 	
 	
 	//Class mère Card
-	public Pokemon(String name, String type) {
+	public Pokemon(String name, String type, int lifepoints, String firstE, String secondE, int placeStage) {
 		//Recup valeur du constructeur mère
 		super(name, type);
+		//param Pokemon
+		lifePoints = lifepoints;
+		firstElement = firstE;
+		secondElement = secondE;
+		stage = placeStage;
 	}
 	
+	//Tab Listes
+	public String Elements[] = {"grass","fire","water","lighting","psychic","fighting","darkness","metal", "fairy","dragon","colorless","null"};
 	
+	
+
 }
